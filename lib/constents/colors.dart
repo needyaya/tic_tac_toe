aa286a4e3da4a25bb5e0fab4425b84b07fa75044
AYA <ayaabouzoul@gmail.com>
2023-04-28 11:47:34 +0100

    init commit

---
+++ lib/constents/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 243, 116, 175);
+  static Color secondaryColor = Color.fromARGB(255, 214, 103, 198);
+  static Color accentColor = Color.fromARGB(255, 160, 201, 251);
+}
